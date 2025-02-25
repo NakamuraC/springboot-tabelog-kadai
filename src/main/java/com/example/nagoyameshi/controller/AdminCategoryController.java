74f3faf3e940902a9f9d8655694dc119df3386f5
NakamuraC <qianxiadaqiao45@gmail.com>
2025-02-25 16:03:08 +0900

    category.idに変更後

--- src/main/java/com/example/nagoyameshi/controller/AdminCategoryController.java
+++ src/main/java/com/example/nagoyameshi/controller/AdminCategoryController.java
@@ -4,35 +4,63 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.nagoyameshi.entity.Category;
+import com.example.nagoyameshi.form.CategoryRegisterForm;
 import com.example.nagoyameshi.repository.CategoryRepository;
+import com.example.nagoyameshi.service.CategoryService;
 
 @Controller
 @RequestMapping("/admin/categories")
 public class AdminCategoryController {
 	private final CategoryRepository categoryRepository;
+	private final CategoryService categoryService;
 
-	public AdminCategoryController(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;                
-    }
+	public AdminCategoryController(CategoryRepository categoryRepository, CategoryService categoryService) {
+		this.categoryRepository = categoryRepository;
+		this.categoryService = categoryService;
+	}
 
 	@GetMapping
 	public String index(Model model, @RequestParam(name = "keyword", required = false) String keyword) {
 		List<Category> categories;
 
 		if (keyword != null && !keyword.trim().isEmpty()) {
-		    categories = categoryRepository.findByNameContaining(keyword);
+			categories = categoryRepository.findByNameContaining(keyword);
 		} else {
-		    categories = categoryRepository.findAll();
+			categories = categoryRepository.findAll();
 		}
-		
+
 		model.addAttribute("categories", categories);
 		model.addAttribute("keyword", keyword);
 
 		return "admin/categories/adminCategory";
+	}
+
+	@GetMapping("/register")
+	public String register(Model model) {
+		model.addAttribute("categoryRegisterForm", new CategoryRegisterForm());
+		return "admin/categories/register";
+	}
+
+	@PostMapping("/create")
+	public String create(@ModelAttribute @Validated CategoryRegisterForm categoryRegisterForm, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes) {
+		if (bindingResult.hasErrors()) {
+			return "admin/categories/register";
+		}
+
+		categoryService.create(categoryRegisterForm);
+		redirectAttributes.addFlashAttribute("successMessage", "カテゴリーを登録しました。");
+
+		return "redirect:/admin/categories";
 	}
 }
